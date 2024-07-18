7e66c396bf2b98494814a56491a5b3c910823c64
IngAlim2023 <enrique.porras@udea.edu.co>
2024-07-18 11:52:24 -0500

    Consumo API: Nombres y Imagenes

--- api/script.js
+++ api/script.js
@@ -1,11 +1,13 @@
 const apiUrl = "https://rickandmortyapi.com/api/character";
 let btn = document.getElementById('ObtApi');
+let cuerp = document.getElementById('cuerpo');
 
 async function getCharacters (){
     try{
         const response = await fetch(apiUrl);
         const resultApi = await response.json(); 
         console.log(resultApi);
+        contruir(resultApi);
     }
     catch(error){
         console.error(error)
@@ -17,4 +19,19 @@ async function getCharacters (){
 
 btn.addEventListener('click', ()=>{
     return getCharacters();
-})
+});
+
+function contruir (resultApi){
+    const longitud = resultApi.results.length;
+
+    for (let i=0; i<longitud; i++){
+        
+        let headhtml = document.createElement('h1');
+        headhtml.innerText = resultApi.results[i].name;
+        cuerp.appendChild(headhtml);
+        let imagenes = document.createElement('img');
+        imagenes.src = resultApi.results[i].image;
+        cuerp.appendChild(imagenes);
+    }
+    
+}
